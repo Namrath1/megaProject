9a3499c948c4f7969eded300c2cbbea450874735
Namrath <namrathracharla1@gmail.com>
2024-03-23 17:49:22 +0530

    forwardref

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import {login, logout} from "./store/authStore"
 
 function App() {
- 
+ const [loading, setloading] = useState(true);
+ const dispatch = useDispatch()
 
-  return (
-    <>
-      
-    </>
-  )
+ useEffect(() => {
+  authService.getCurrentUser()
+  .then((user)=>{
+    if(user){
+      dispatch(login({user}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>{setloading(false)})
+ }, []);
+
+  return !loading ? (<div className="min-h-screen flex flex-wrap text-center bg-gray-400">
+   <div className="w-full block ">
+    <Header/>
+    <main>
+      <Outlet/>
+    </main>
+    <Footer/>
+   </div>
+  </div>): (null)
+  
 }
 
 export default App
